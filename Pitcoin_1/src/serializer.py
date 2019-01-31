bee6a34a8be92b14c8e148b4bd558c3ade1c6bd4
kloffer <ilyakachko@gmail.com>
2019-01-31 19:19:02 +0200

    serializer and deserializer done, need to test them more and reorganize for classes

--- Pitcoin_1/src/serializer.py
+++ Pitcoin_1/src/serializer.py
@@ -21,23 +21,26 @@ class Deserializer:
         pub_key = transaction[72:202]
         signature = transaction[202:]
         return dict({
-            'num_of_coins' : num_of_coins,
-            'sender_addr' : send_addr,
-            'recepient_addr' : recipient_addr,
-            'public_key' : pub_key,
-            'signature' : signature
+            'num_of_coins': num_of_coins,
+            'sender_addr': send_addr,
+            'recepient_addr': recipient_addr,
+            'public_key': pub_key,
+            'signature': signature
                     })
+
 
 transaction = '01000000017967a5185e907a25225574544c31f7b059c1a191d65b53dcc1554d339c4f9efc010000006a47304402206a2eb16b7b92051d0fa38c133e67684ed064effada1d7f925c842da401d4f22702201f196b10e6e4b4a9fff948e5c5d71ec5da53e90529c8dbd122bff2b1d21dc8a90121039b7bcd0824b9a9164f7ba098408e63e5b7e3cf90835cceb19868f54f8961a825ffffffff014baf2100000000001976a914db4d1141d0048b1ed15839d0b7a4c488cd368b0e88ac00000000'
 
 version = transaction[0:4]
 # prRed(str(len('47304402206a2eb16b7b92051d0fa38c133e67684ed064effada1d7f925c842da401d4f22702201f196b10e6e4b4a9fff948e5c5d71ec5da53e90529c8dbd122bff2b1d21dc8a90121039b7bcd0824b9a9164f7ba098408e63e5b7e3cf90835cceb19868f54f8961a825')))
 
+
 def swap_bits_in_str(str):
     tmp = ''
     for i in range(0, len(str), 2):
         tmp = str[i] + str[i + 1] + tmp
     return tmp
+
 
 def get_varint(str):
     if str.startswith('fd'):
@@ -55,6 +58,80 @@ def get_varint(str):
     # print(length)
     # print((str[2:length]))
     return length, int(swapped_str, 16)
+
+def make_varint(num):
+    if num <= 0xfc:
+        return swap_bits_in_str('%02x' % num)
+    elif num <= 0xffff:
+        return swap_bits_in_str('fd%04x' % num)
+    elif num <= 0xffffffff:
+        return swap_bits_in_str('fe%08x' % num)
+    elif num <= 0xffffffffffffffff:
+        return swap_bits_in_str('ff%016x' % num)
+
+
+num = 106
+num_fd = 550
+num_fe = 998000
+
+varint = make_varint(num)
+varint_fd = make_varint(num_fd)
+varint_fe = make_varint(num_fe)
+
+print(varint)
+print(varint_fd)
+print(varint_fe)
+
+
+def serialize(version, inputs, outputs, locktime):
+    serialized_tx = ''
+
+    # Version
+    serialized_tx += '%08x' % version
+
+    # Input Count
+    inputs_count = len(inputs)
+    serialized_tx += make_varint(inputs_count)
+
+    # Input(s)
+    for input in inputs:
+        # TXID
+        tx_id = swap_bits_in_str(input.prev_output_hash)
+        serialized_tx += tx_id
+
+        # VOUT
+        vout = swap_bits_in_str(input.prev_output_idx)
+        serialized_tx += vout
+
+        # ScriptSig Size
+        script_sig_size = make_varint(input.script_length)
+        serialized_tx += script_sig_size
+
+        # ScriptSig
+        serialized_tx += input.sign_script
+
+        # Sequence
+        serialized_tx += input.sequence
+
+    # Output Count
+    outputs_count = len(outputs)
+    serialized_tx += make_varint(outputs_count)
+
+    # Output(s)
+    for output in outputs:
+        # Value
+        value = swap_bits_in_str('%04x' % output.value)
+        serialized_tx += value
+
+        # ScriptPubKey Size
+        script_pubkey_size = make_varint(output.script_length)
+        serialized_tx += script_pubkey_size
+
+        # ScriptPubKey
+        serialized_tx += output.pubkey_script
+
+    serialized_tx += swap_bits_in_str('%04x' % locktime)
+
 
 def deserialize(serialized_tx):
 
